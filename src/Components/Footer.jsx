035147f20f107ac389777722cc06a31caf322c8c
mohammad-nayem <nishanahmed13913@gmail.com>
2025-02-05 21:09:42 +0600

    created footer

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -3,7 +3,15 @@ import React from 'react';
 const Footer = () => {
     return (
         <>
-            <h1 className='text-danger'>This is Footer</h1>
+        <footer className='bg-primary-subtle'>
+            <div className="container py-4">
+                <div className="row">
+                    <div className="col-12 d-flex align-items-center justify-content-center ">
+                        <p  className='text-center text-secondary  mb-0'>All right reserve @Nayem Ahmed</p>
+                    </div>
+                </div>
+            </div>
+        </footer>
         </>
     );
 };
