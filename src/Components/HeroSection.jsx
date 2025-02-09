f13b78d619bf5e454ad22177fa786c22ff25550c
mohammad-nayem <nishanahmed13913@gmail.com>
2025-02-09 21:32:35 +0600

    modified Hero area style and added animation in image side

--- src/Components/HeroSection.jsx
+++ src/Components/HeroSection.jsx
@@ -15,24 +15,23 @@ const HeroSection = () => {
                         <div className="col-md-6 col-lg-6 col-12 py-5">
                             <div>
                                 <h4 className='text-secondary'>Hi, I'm</h4>
-                                <h1 className='h1_color fw-bold '>
-                                   
+                                <h1 className='h1_color fw-bold '>     
                                    {/* Added text animation  */}
-                                <TypeAnimation
-                                         sequence={[
-                                             'Nayem Ahmed', // Types 'One'
-                                             2000, // Waits 1s
-                                             'A Full Stack Javascript Developer', // Deletes 'One' and types 'Two'
-                                             2000, // Waits 2s
-                                            ]}
-                                            speed={40}
-                                            wrapper="span"
-                                        cursor={true}
-                                        repeat={Infinity}
-                                        style={{ fontSize: '1.2em', display: 'inline-block' }}
-                                        />
+                                    <TypeAnimation
+                                            sequence={[
+                                                'Nayem Ahmed', // Types 'One'
+                                                2000, // Waits 1s
+                                                'A Full Stack Javascript Developer', // Deletes 'One' and types 'Two'
+                                                2000, // Waits 2s
+                                                ]}
+                                                speed={40}
+                                                wrapper="span"
+                                            cursor={true}
+                                            repeat={Infinity}
+                                            style={{ fontSize: '1.2em', display: 'inline-block' }}
+                                    />
                                     
-                                    </h1>
+                                </h1>
                                 <p className='py-3 text-secondary'>
                                     I’m a full-stack developer who loves crafting clean, scalable web applications. My goal is to build solutions that offer both exceptional performance and a delightful user experience.
                                 </p>
@@ -41,7 +40,7 @@ const HeroSection = () => {
                         </div>
                         <div className="pt-sm-5 col-md-6 col-lg-6 col-12 ">
                             <div className='w-100 h-100 d-block d-flex justify-content-center align-items-center'>
-                                <img className='_shadow img-fluid w-250px w-md-70 rounded rounded-circle bg-dark' src={image} alt="Photo" />
+                                <img className='_shadow bg-color animate img-fluid w-50 rounded rounded-circle ' src={image} alt="Photo" />
                             </div>
                         </div>
                     </div>
