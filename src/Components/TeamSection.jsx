fdb383f90c603d694ebfca145d4e5720e98a58a6
mohammad-nayem <nishanahmed13913@gmail.com>
2025-02-09 23:34:42 +0600

    Created Team section in about page

---
+++ src/Components/TeamSection.jsx
@@ -0,0 +1,76 @@
+import React from 'react';
+
+import teamImage from '../assets/image/me.png'
+
+
+
+const TeamSection = () => {
+    return (
+        <>
+          <section className='py-5'>
+            <div className="container vh-100">
+                <div className="row">
+                    <div className="col-md-12 text-center">
+                        <div>
+                            <h1 className='h1_color text-center mb-0 fw-bold'>Meet Our Team</h1>
+                        </div>
+                    </div>
+                    <div className="col-md-12 d-flex gap-4 pt-5">
+                        
+                        <div className="team-card ratio ratio-1x1 bg-warning rounded rounded-sm">
+                            <img className='w-100 object-fit-cover rounded rounded-sm' src={teamImage} alt="Nayem"/>
+                            <div className="hover-layer">
+                                <div className="text">
+                                    <h5 className=''>Nayem Ahmed</h5>
+                                    <p className='text-info'>Web Developer</p>
+                                    <p className='text-color'>1 Year experiance in web development</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="team-card ratio ratio-1x1 bg-warning rounded rounded-sm">
+                            <img className='w-100 object-fit-cover rounded rounded-sm' src={teamImage} alt="Nayem"/>
+                            <div className="hover-layer">
+                                <div className="text">
+                                    <h5 className=''>Nayem Ahmed</h5>
+                                    <p className='text-info'>Web Developer</p>
+                                    <p className='text-color'>1 Year experiance in web development</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="team-card ratio ratio-1x1 bg-warning rounded rounded-sm">
+                            <img className='w-100 object-fit-cover rounded rounded-sm' src={teamImage} alt="Nayem"/>
+                            <div className="hover-layer">
+                                <div className="text">
+                                    <h5 className=''>Nayem Ahmed</h5>
+                                    <p className='text-info'>Web Developer</p>
+                                    <p className='text-color'>1 Year experiance in web development</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="team-card ratio ratio-1x1 bg-warning rounded rounded-sm">
+                            <img className='w-100 object-fit-cover rounded rounded-sm' src={teamImage} alt="Nayem"/>
+                            <div className="hover-layer">
+                                <div className="text">
+                                    <h5 className=''>Nayem Ahmed</h5>
+                                    <p className='text-info'>Web Developer</p>
+                                    <p className='text-color'>1 Year experiance in web development</p>
+                                </div>
+                            </div>
+                        </div>
+
+                       
+                    </div>
+                </div>
+            </div>
+          </section>
+        </>
+    );
+};
+
+export default TeamSection;
+
+
+
+/*
+   
+*/
--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Layout from '../Layout/Layout';
 import AboutSection from '../Components/AboutSection';
+import TeamSection from '../Components/TeamSection';
 
 const About = () => {
     return (
         <Layout>
              <AboutSection />
+             <TeamSection />
         </Layout>
     );
 };
