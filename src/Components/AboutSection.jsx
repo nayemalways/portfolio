191a3f2d208140d93f02fe90ad8698eb7b64881f
mohammad-nayem <nishanahmed13913@gmail.com>
2025-02-09 21:44:48 +0600

    remove comment code

--- src/Components/AboutSection.jsx
+++ src/Components/AboutSection.jsx
@@ -28,31 +28,3 @@ const AboutSection = () => {
 };
 
 export default AboutSection;
-
-
-/*
-  <section>
-            <div style={{height: "90vh"}} className="container  ">
-                <div className="row h-100 d-flex justify-content-center align-items-center">
-                    <div className="col-md-12">
-                        <h1 className='h1_color text-center'>About Me</h1>
-                    </div>
-                    <div className="col-md-12">
-                        <div className="row d-flex justify-content-center align-items-center">
-                            <div className="col-sm-12 col-md-6">
-                                <p>
-                                Hello! I'm Nayem, a passionate Junior Full-Stack MERN Developer.
-                                I specialize in building dynamic and responsive web applications using MongoDB, Express.jsReact, and Node.js. With a strong foundation in JavaScript and experience in development, I enjoy crafting seamless and efficient web experiences.
-
-                                I'm always eager to learn new technologies and improve my skills. My goal is to scalable and user-friendly applications that solve real-world problems.
-                                </p>
-                            </div>
-                            <div className="col-sm-12 col-md-6 text-center">
-                                <img style={{maxWidth: "200px"}} className='img-fluid ' src={image} alt="Photo" />
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-          </section> 
-*/
