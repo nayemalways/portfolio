f0e55ba98a74d8d84ad173cb96003aee975f30ea
mohammad-nayem <nishanahmed13913@gmail.com>
2025-02-10 00:45:32 +0600

    Contact form modified

--- src/Components/ContactForm.jsx
+++ src/Components/ContactForm.jsx
@@ -6,20 +6,19 @@ const ContactForm = () => {
             <section className='py-5'>
                 <div style={{height: '100vh'}} className="container">
                     <div className="row d-flex justify-content-center align-items-center">
-                        <div className="col-md-12 text-center">
-                            <h2 className='h1_color fw-bold'>Get in touch</h2>
-                        </div>
-                        <div className="col-md-6 text-start">
+        
+                        <div className="col-md-6 text-start   bg-dark p-5 rounded rounded-4">
+                            <h2 className='h1_color fw-bold text-center'>Get in touch</h2>
                             <form id='form' action="">
                                 <div className='pt-3'>
                                     <input className='form-control text-white' id='name' name='name' type="text" placeholder='Name..'/>
                                 </div>                            
                                 <div className='pt-3'>
-                                    <input className='form-control' id='email' name='email' type="email" placeholder='example@gmail.com'/>
+                                    <input className='form-control text-white' id='email' name='email' type="email" placeholder='example@gmail.com'/>
                                 </div>
 
                                 <div className='py-3'>
-                                    <textarea rows={6} className='form-control' name="message" id="message" placeholder='your message..'></textarea>
+                                    <textarea rows={6} className='form-control text-white' name="message" id="message" placeholder='your message..'></textarea>
                                 </div>
                                 <button type='submit' className='btn btn-primary w-100'>Send Message</button>
                             </form>
