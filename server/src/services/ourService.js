01e073b87802ae79d576c2f4cc51752a9572836e
mohammad-nayem <nishanahmed13913@gmail.com>
2025-02-11 23:04:40 +0600

    Our service Api function setup

---
+++ server/src/services/ourService.js
@@ -0,0 +1,41 @@
+
+
+
+// Service create
+export const serviceCreate = async (req) => {
+    try {
+
+    }catch(e) {
+        console.log(e.toString());
+    }
+}
+
+
+// Service read
+export const serviceRead = async (req) => {
+    try {
+
+    }catch(e) {
+        console.log(e.toString());
+    }
+}
+
+// Service update
+export const serviceUpdate = async (req) => {
+    try {
+
+    }catch(e) {
+        console.log(e.toString());
+    }
+}
+
+
+// Service delete
+export const serviceDelete = async (req) => {
+    try {
+
+    }catch(e) {
+        console.log(e.toString());
+    }
+}
+
