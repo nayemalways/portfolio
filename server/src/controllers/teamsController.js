7f133dccf67016e9007568a5460a88f4b40074e9
mohammad-nayem <nishanahmed13913@gmail.com>
2025-02-12 00:30:56 +0600

    Added comment in Team controllers

--- server/src/controllers/teamsController.js
+++ server/src/controllers/teamsController.js
@@ -2,21 +2,27 @@ import { createMemberService, deleteMemberService, readMemberService, updateMemb
 
 
 
+// Create member controller
 export const createMember = async (req, res) => {
     const result = await createMemberService(req);
     res.json({result});
 }
 
+
+// Read member controller
 export const readMember = async (req, res) => {
     const result = await readMemberService();
     res.json({result});
 }
 
+
+// Update member controller
 export const updateMember = async (req, res) => {
     const result = await updateMemberService(req);
     res.json({result});
 }
 
+// Delete member controller
 export const deleteMember = async (req, res) => {
     const result = await deleteMemberService(req);
     res.json({result});
