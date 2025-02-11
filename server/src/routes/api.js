e09cc73fe3ec583161952a65cc03511d4137cc7f
mohammad-nayem <nishanahmed13913@gmail.com>
2025-02-12 00:04:36 +0600

    Set up Team section Api created related function and Api endpoint

--- server/src/routes/api.js
+++ server/src/routes/api.js
@@ -1,9 +1,13 @@
 import express from "express";
+import { userAuthentication } from './../middleware/authMiddleware.js';
 import * as userController from "../controllers/userController.js";
 import  * as blogController from "../controllers/blogController.js";
-import { userAuthentication } from './../middleware/authMiddleware.js';
 import * as ourServiceController from "../controllers/ourServiceController.js";
+import * as teamController from "../controllers/teamsController.js";
  
+
+
+
 // Express Router
 const router = express.Router();
 
@@ -24,6 +28,12 @@ router.post('/create-service', userAuthentication, ourServiceController.createSe
 router.get('/read-service', userAuthentication, ourServiceController.readService);
 router.post('/update-service/:serviceId', userAuthentication, ourServiceController.updateService );
 router.get('/delete-service/:serviceId', userAuthentication, ourServiceController.deleteService );
+
+// Team member related API
+router.post('/create-member', userAuthentication, teamController.createMember);
+router.get('/read-member', userAuthentication, teamController.readMember);
+router.post('/update-member', userAuthentication, teamController.updateMember);
+router.get('/update-member', userAuthentication, teamController.deleteMember);
 
 
 // Team related APi
---
+++ server/src/services/teamsService.js
@@ -0,0 +1,39 @@
+
+
+
+export const createMemberService = async (req) => {
+    try {
+
+    }catch(e) {
+        console.log(e.toString());
+        return {status: "error", message: "Internal server error"}
+    }
+}
+
+export const readMemberService = async () => {
+    try {
+
+    }catch(e) {
+        console.log(e.toString());
+        return {status: "error", message: "Internal server error"}
+    }
+}
+
+export const updateMemberService = async (req) => {
+    try {
+
+    }catch(e) {
+        console.log(e.toString());
+        return {status: "error", message: "Internal server error"}
+    }
+}
+
+export const deleteMemberService = async (req) => {
+    try {
+
+    }catch(e) {
+        console.log(e.toString());
+        return {status: "error", message: "Internal server error"}
+    }
+}
+
