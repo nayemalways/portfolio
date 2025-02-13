37ffcf349a21e5827478a59e652e8e88587613c5
mohammad-nayem <nishanahmed13913@gmail.com>
2025-02-13 12:18:57 +0600

    refactor ServiceCard

--- client/src/Components/ServiceCard.jsx
+++ client/src/Components/ServiceCard.jsx
@@ -9,7 +9,7 @@ import iconImage from '../assets/image/ui-ux.png'
 const ServiceCard = () => {
     return (
         <>
-            <div className="card rounded rounded-4" style={{minWidth: "16rem", maxWidth: '16rem', height: '20rem'}}>
+            <div className="card rounded rounded-4" style={{minWidth: "16rem", maxWidth: '16rem', height: 'auto'}}>
                 <div className="card-body">
                     <img style={{width: "25px"}} className='py-3' src={iconImage} alt="" />
                     <h5 className="card-title text-light">UI/UX design</h5>
