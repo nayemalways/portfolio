762ce88aa7cf7effd116b2c4a478b9e3c8744a13
mohammad-nayem <nishanahmed13913@gmail.com>
2025-02-13 23:03:58 +0600

    Added form in Dashboard service section

--- client/src/Pages/Dashboard/DashService.jsx
+++ client/src/Pages/Dashboard/DashService.jsx
@@ -1,11 +1,48 @@
 import React from 'react';
-
- import DashboardLayout from '../../Components/AdminDashboard/DashboardMasterLayout/DashboardLayout';
+import DashboardLayout from '../../Components/AdminDashboard/DashboardMasterLayout/DashboardLayout';
+ 
 
 const DashService = () => {
     return (
         <DashboardLayout>
-            <h1>This is service page</h1>
+             <section>
+                <div className="container">
+                    <div className="row d-flex justify-content-center align-content-center h-auto py-5">
+                        <div className="col-md-8">
+                            <h2 className='color text-center mb-5'>Add Services</h2>
+                             
+                            <form>
+                                <div className="container">
+                                    <div className="row">
+                                        
+                                        <div className="col-md-6 mt-3">
+                                            <label className='form-label' htmlFor="fullName">Service Name:</label>
+                                            <input className='form-control' type="text" name="fullName" id="fullName" placeholder='Your service name..' />
+                                        </div>
+                                        
+                                        <div className="col-md-6 mt-3">
+                                            <label className='form-label' htmlFor="experience">Service Brief:</label>
+                                            <input className='form-control' type="text" name="experience" id="experience" placeholder='Explain about your service' />
+                                        </div>
+
+                                        <div className="col-md-6 mt-3">
+                                            <label className='form-label' htmlFor="image">Image:</label>
+                                            <input className='form-control' type="file" name="image" id="image"  />
+                                        </div>
+                                        
+                                        <div className="col-md-12 mt-3">
+                                            <button className='w-100 fw-bold fs-5 btn color btn-dark' type='submit'>Send</button>
+                                        </div>
+
+                                        
+                                        
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+             </section>
         </DashboardLayout>
     );
 };
