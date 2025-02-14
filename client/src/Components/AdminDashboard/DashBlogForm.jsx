90ec1f475ecb09cd1be0f7916a77f7e6901dfbbd
mohammad-nayem <nishanahmed13913@gmail.com>
2025-02-14 11:48:38 +0600

    Integrate Text Editor in Blog post section in Dashboard

---
+++ client/src/Components/AdminDashboard/DashBlogForm.jsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import { useQuill } from 'react-quilljs';
+import 'quill/dist/quill.snow.css';
+
+const DashBlogForm = () => {
+    const { quill, quillRef } = useQuill();
+
+    return (
+        <>
+            <form>
+                <div className="container">
+                    <div className="row">
+                                        
+                        <div className="col-md-12  mt-3">
+                            <label className='form-label' htmlFor="fullName">Title:</label>
+                            <input className='form-control' type="text" name="fullName" id="fullName" placeholder='Enter title here..' />
+                        </div>
+
+
+                            <div className='col-md-12 my-5'>
+                                <div style={{ width: '100%', height: "25rem" }} className=' text-dark'>
+                                    <div ref={quillRef} />
+                                </div>
+                            </div>
+
+                            
+                            <div className="col-md-6 mt-3">
+                                <label className='form-label' htmlFor="image">Image:</label>
+                                <input className='form-control' type="file" name="image" id="image"  />
+                            </div>
+
+                                        
+                        <div className="col-md-12 mt-5">
+                            <button className='w-100 fw-bold fs-5 btn color btn-dark' type='submit'>Send</button>
+                        </div>
+
+                                        
+                                        
+                    </div>
+                </div>
+            </form>
+        </>
+    );
+};
+
+export default DashBlogForm;
--- client/src/Pages/Dashboard/DashBlogs.jsx
+++ client/src/Pages/Dashboard/DashBlogs.jsx
@@ -1,11 +1,24 @@
 import React from 'react';
 
-  import DashboardLayout from '../../Components/AdminDashboard/DashboardMasterLayout/DashboardLayout';
+
+import DashboardLayout from '../../Components/AdminDashboard/DashboardMasterLayout/DashboardLayout';
+import DashBlogForm from '../../Components/AdminDashboard/DashBlogForm';
 
 const DashBlogs = () => {
+
     return (
         <DashboardLayout>
-            <h1>This is blog page</h1>
+              <section>
+                <div className="container">
+                    <div className="row d-flex justify-content-center align-content-center h-auto py-5">
+                        <div className="col-md-10">
+                            <h2 className='color text-center mb-5'>Add Blogs</h2>
+                             
+                            <DashBlogForm />
+                        </div>
+                    </div>
+                </div>
+             </section>
         </DashboardLayout>
     );
 };
