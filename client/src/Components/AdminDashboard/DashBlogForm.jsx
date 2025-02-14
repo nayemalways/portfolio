7c2a71803d36c492145f8f698404457753a43430
mohammad-nayem <nishanahmed13913@gmail.com>
2025-02-14 20:33:30 +0600

    dashboard Blog section styled

--- client/src/Components/AdminDashboard/DashBlogForm.jsx
+++ client/src/Components/AdminDashboard/DashBlogForm.jsx
@@ -18,20 +18,20 @@ const DashBlogForm = () => {
 
 
                             <div className='col-md-12 my-5'>
-                                <div style={{ width: '100%', height: "25rem" }} className=' text-dark'>
+                                <div style={{ width: '100%', height: "25rem" }} className='text-light rounded rounded-sm '>
                                     <div ref={quillRef} />
                                 </div>
                             </div>
 
                             
-                            <div className="col-md-6 mt-3">
+                            <div className="col-md-6 mt-5">
                                 <label className='form-label' htmlFor="image">Image:</label>
                                 <input className='form-control' type="file" name="image" id="image"  />
                             </div>
 
                                         
-                        <div className="col-md-12 mt-5">
-                            <button className='w-100 fw-bold fs-5 btn color btn-dark' type='submit'>Send</button>
+                        <div className="col-md-12 mt-3">
+                            <button className='w-100 fw-bold fs-5 btn color btn-secondary' type='submit'>Send</button>
                         </div>
 
                                         
--- client/src/Pages/Dashboard/DashBlogs.jsx
+++ client/src/Pages/Dashboard/DashBlogs.jsx
@@ -11,9 +11,8 @@ const DashBlogs = () => {
               <section>
                 <div className="container">
                     <div className="row d-flex justify-content-center align-content-center h-auto py-5">
-                        <div className="col-md-10">
+                        <div className="col-md-10 bg-dark p-5 border border-secondary rounded rounded-4">
                             <h2 className='color text-center mb-5'>Add Blogs</h2>
-                             
                             <DashBlogForm />
                         </div>
                     </div>
