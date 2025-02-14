2a86a4d7064cbdb0a99cee77533a8d162b3e3aa0
mohammad-nayem <nishanahmed13913@gmail.com>
2025-02-14 20:55:34 +0600

    Dashboard Service section responsive

--- client/src/Pages/Dashboard/DashService.jsx
+++ client/src/Pages/Dashboard/DashService.jsx
@@ -11,26 +11,26 @@ const DashService = () => {
                         <div  className="col-lg-8 col-md-8 col-10 border border-secondary rounded rounded-4 p-3 bg-dark">
                             <h2 className='color text-center mb-4'>Add Services</h2>
                              
-                            <form id='form'>
+                            <form id='form' className='p-sm'>
                                 <div className="container">
                                     <div className="row">
                                         
-                                        <div className="col-md-8 m-auto mt-3">
+                                        <div className="col-lg-8 col-md-10 m-auto mt-3">
                                             <label className='form-label text-start d-block' htmlFor="serviceName">Service Name:</label>
                                             <input className='form-control' type="text" name="serviceName" id="serviceName" placeholder='Your service name..' />
                                         </div>
                                         
-                                        <div className="col-md-8 m-auto mt-3">
+                                        <div className="col-lg-8 col-md-10 m-auto mt-3">
                                             <label className='form-label text-start d-block' htmlFor="serviceDetails">Service Explain:</label>
                                             <textarea className='form-control' cols={30} rows={7} name="serviceDetails" id="serviceDetails" placeholder='Explain about your services...'></textarea>
                                          </div>
 
-                                        <div className="col-md-8 m-auto mt-3">
+                                        <div className="col-lg-8 col-md-10 m-auto mt-3">
                                             <label className='form-label text-start d-block' htmlFor="image">Image:</label>
                                             <input className='form-control' type="file" name="image" id="image"  />
                                         </div>
                                         
-                                        <div className="col-md-8 m-auto mt-3">
+                                        <div className="col-lg-8 col-md-10 m-auto mt-3">
                                             <button className='w-100 fw-bold fs-5 btn color btn-secondary' type='submit'>Send</button>
                                         </div>
 
