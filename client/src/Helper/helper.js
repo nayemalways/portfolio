e8d0fd4de500da62aaf197587beba3a3d8469cfe
mohammad-nayem <nishanahmed13913@gmail.com>
2025-02-17 22:21:26 +0600

    Set up cloudinary image uploade setting

--- client/src/Helper/helper.js
+++ client/src/Helper/helper.js
@@ -1,3 +1,6 @@
+import axios from "axios";
+import dotenv from "dotenv";
+dotenv.config();
 
 
 export function getBase64 (file, callback) {
@@ -7,4 +10,27 @@ export function getBase64 (file, callback) {
     reader.addEventListener('load', () => callback(reader.result));
 
     reader.readAsDataURL(file);
+}
+
+
+
+// Cloudinary set up
+export const cloudinaryImageUpload = async (file) => {
+    const data = new FormData();
+    data.append(file, 'image');
+    data.append('upload_preset', 'Images_preset');
+
+    try {
+
+        let cloudName = process.env.cloudinaryName;
+        let api = `https://api.cloudinary.com/v1_1/${cloudName}/image/upload`;
+
+        const response = await axios.post(api, data);
+        const { secure_url } = response.data;
+        console.log(secure_url);
+        return secure_url;
+
+    }catch(e) {
+        console.error(e);
+    }
 }
