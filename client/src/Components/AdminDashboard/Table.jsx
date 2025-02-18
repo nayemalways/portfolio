eb3e7ccf9aa97e24c0fb6504cec8874a6e9b1d70
mohammad-nayem <nishanahmed13913@gmail.com>
2025-02-18 19:14:12 +0600

    Changes style

--- client/src/Components/AdminDashboard/Table.jsx
+++ client/src/Components/AdminDashboard/Table.jsx
@@ -11,10 +11,10 @@ const Table = ({title, data}) => {
 
     return (
         <>
-             <div className="col-12 col-md-12 col-lg-12 table-style">
+            <div className="col-12 col-md-12 col-lg-12 table-style">
                 <h3 className='color'>{title}</h3>
-                <div className=' table-responsive-sm p-sm-3 '>
-                    <table className='table table-striped '>
+                <div className='table-responsive p-sm-3'>
+                    <table className='table table-striped'>
                         <thead className='text-center'>
                             <tr>
                                 <th>No:</th>
@@ -25,27 +25,33 @@ const Table = ({title, data}) => {
                             </tr>
                         </thead>
                         <tbody>
-
                             {
-
                                 data.map((item, index) => {
                                     return (
                                         <tr key={index} className='text-center'>
-                                            <td className='align-content-center'>{index + 1}</td>
-                                            <td className='w-25 align-content-center'><img className='table-img rounded rounded-2' src={item.image} alt="" /></td>
-                                            <td  className='text-ellipsis align-content-center'> {item.title || item.name} </td>
-                                            <td><button className='btn btn-danger'><MdDeleteSweep /></button></td>
-                                            <td className='align-content-center'><Link to={`/dashboard/update/${item._id}`} className='d-block btn btn-success'><FaEdit /></Link></td>
+                                            <td className='align-middle'>{index + 1}</td>
+                                            <td className='align-middle'>
+                                                <img className='table-img rounded rounded-2 img-fluid' src={item.image} alt="" />
+                                            </td>
+                                            <td className='text-truncate align-middle' style={{maxWidth: '150px'}}> 
+                                                {item.title || item.name} 
+                                            </td>
+                                            <td className='align-middle'>
+                                                <button className='btn btn-danger'><MdDeleteSweep /></button>
+                                            </td>
+                                            <td className='align-middle'>
+                                                <Link to={`/dashboard/update/${item._id}`} className='d-block btn btn-success'>
+                                                    <FaEdit />
+                                                </Link>
+                                            </td>
                                         </tr>
                                     )
                                 })
-
                             }
-                            
                         </tbody>
                     </table>
                 </div>
-            </div>                    
+            </div>
         </>
     );
 };
