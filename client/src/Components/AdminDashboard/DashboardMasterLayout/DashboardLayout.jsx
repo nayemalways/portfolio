79642158e3872bc0cf08b0d4033d55be1693e627
mohammad-nayem <nishanahmed13913@gmail.com>
2025-02-18 17:30:22 +0600

    Removed unnecessary css class

--- client/src/Components/AdminDashboard/DashboardMasterLayout/DashboardLayout.jsx
+++ client/src/Components/AdminDashboard/DashboardMasterLayout/DashboardLayout.jsx
@@ -1,16 +1,16 @@
 import React from 'react';
 import Sidebar from '../Sidebar';
- 
+
 const DashboardLayout = ({children}) => {
     return (
         < >
-            <section>
-                <div className="container-fluid">
+            <section >
+                <div className="container-fluid ">
                     <div className="row">
                         <div className="col-md-2 col-lg-2 col-2 m-0 p-0 ">
                              <Sidebar/>
                         </div>
-                        <div className="col-md-10 col-lg-10 col-10 vh-100   p-0">
+                        <div  className="col-md-10 col-lg-10 col-10 vh-100 p-0 ">
 
 
                               {children}
