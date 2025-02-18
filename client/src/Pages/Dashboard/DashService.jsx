308d933d452624da034c73214a2bcb83fea88137
mohammad-nayem <nishanahmed13913@gmail.com>
2025-02-18 17:27:43 +0600

    Responsive Form

--- client/src/Pages/Dashboard/DashService.jsx
+++ client/src/Pages/Dashboard/DashService.jsx
@@ -52,10 +52,8 @@ const DashService = () => {
             const image = e.target.files[0];
     
             if (image) {
-                 
                 // Upload image to cloud
                 const result = await cloudinaryImageUpload(image);
-
                 if(result) {
                     toast.success('Image uploaded');
                 }else{
@@ -75,16 +73,7 @@ const DashService = () => {
     
 
 
-  /*  const handleImage = async (e) => {
-        try{
-            const image = e.target.files[0];
-            const result = await cloudinaryImageUpload(image);
-            formik.setFieldValue('image', result);
-            e.target.value = '';
-        }catch(e) {
-            console.log(e);
-        }
-    }*/
+  
 
 
     return (
@@ -95,65 +84,65 @@ const DashService = () => {
                     loader && <Loader/>
                 }
 
-                <div className="container ">
+
+                <div className="container">
                     <div className="row d-flex justify-content-center align-content-center h-auto py-5">
-                        <div  className="col-lg-8 col-md-8 col-10 border border-secondary rounded rounded-4 p-3 bg-dark">
+                        <div className="col-lg-8 col-md-10 col-12 border border-secondary rounded rounded-4 p-3 bg-dark">
                             <h2 className='color text-center mb-4'>Add Services</h2>
-                             
+                            
                             <form onSubmit={formik.handleSubmit} id='form' className='p-sm'>
                                 <div className="container">
                                     <div className="row">
                                         
-                                        <div className="col-lg-8 col-md-10 m-auto mt-3">
+                                        <div className="col-12 mt-3">
                                             <label className='form-label text-start d-block' htmlFor="title">Title :</label>
                                             <input 
                                                 onChange={formik.handleChange} 
                                                 value={formik.values.title} 
-                                                className='form-control' 
+                                                className='form-control w-100' 
                                                 type="text"
                                                 name="title" id="title" placeholder='Your service name..' />
                                                 {formik.touched.title && formik.errors.title && <span style={{color: "red"}}>{formik.errors.title}</span>}
                                         </div>
                                         
-                                        <div className="col-lg-8 col-md-10 m-auto mt-3">
+                                        <div className="col-12 mt-3">
                                             <label className='form-label text-start d-block' htmlFor="description">Description :</label>
                                             <textarea 
                                                 onChange={formik.handleChange} 
                                                 value={formik.values.description}  
-                                                className='form-control' 
+                                                className='form-control w-100' 
                                                 cols={30} rows={7} 
                                                 name="description" 
                                                 id="description" 
                                                 placeholder='Explain about your services...'></textarea>
                                                 {formik.touched.description && formik.errors.description && <span style={{color: "red"}}>{formik.errors.description}</span>}
 
-                                         </div>
+                                        </div>
 
-                                        <div className="col-lg-8 col-md-10 m-auto mt-3">
+                                        <div className="col-12 mt-3">
                                             <label className='form-label text-start d-block' htmlFor="image">Image :</label>
                                             <input
                                                 onChange={handleImage} 
                                                 accept="image/*" 
-                                                className='form-control' 
+                                                className='form-control w-100' 
                                                 type="file" 
                                                 name="image" 
                                                 id="image"  />
                                                 {formik.touched.image && formik.errors.image && <span style={{color: "red"}}>{formik.errors.image}</span>}
-
                                         </div>
                                         
-                                        <div className="col-lg-8 col-md-10 m-auto mt-3">
+                                        <div className="col-12 mt-3">
                                             <button className='w-100 fw-bold btn color btn-secondary' type='submit'>Send</button>
                                         </div>
 
-                                        
-                                        
                                     </div>
                                 </div>
                             </form>
                         </div>
                     </div>
                 </div>
+
+                 
              </section>
         </DashboardLayout>
     );
