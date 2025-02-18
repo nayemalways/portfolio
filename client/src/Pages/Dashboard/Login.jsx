4c358fecfbef6460ffd6f2895741d4f87f4e2fb8
mohammad-nayem <nishanahmed13913@gmail.com>
2025-02-18 21:06:55 +0600

    Integrated Login api

--- client/src/Pages/Dashboard/Login.jsx
+++ client/src/Pages/Dashboard/Login.jsx
@@ -1,6 +1,46 @@
 import React from 'react';
+import { useFormik } from 'formik';
+import toast from 'react-hot-toast';
+import Cookies from 'js-cookie';
+
+import { ApiRequest } from '../../ApiRequest/Api';
+import { useNavigate } from 'react-router-dom';
+
+
 
 const Login = () => {
+
+    // Page navigatin hook
+    const navigate = useNavigate();
+
+
+    // Form handle using formik
+    const formik = useFormik({
+        initialValues: {
+            email: '',
+            password: ''
+        },
+
+        onSubmit: async (values, {resetForm}) => {
+
+            // Hit Login API
+            const {status, message, Token} = await ApiRequest("POSt", '/login', values);
+
+            if(status === 'success') {
+                Cookies.set('token', Token , { expires: 30 }); // Set cookie
+                toast.success(message);
+                navigate('/')
+            }else {
+                toast.error(message);
+            }
+
+            // Clear form
+             resetForm();
+        }
+    })
+
+
+
     return (
         <>
         <section>
@@ -8,17 +48,30 @@ const Login = () => {
                 <div className="row vh-100 d-flex justify-content-center align-items-center"> 
                     <div className="col-12 d-flex justify-content-center">
 
-                        <form className='col-12 col-md-5 col-lg-5 bg-dark p-4 rounded rounded-3 border border-secondary '>
+                        <form onSubmit={formik.handleSubmit} className='col-12 col-md-5 col-lg-5 bg-dark p-4 rounded rounded-3 border border-secondary '>
                             <div className="container">
                                 <h1 className='h1_color text-center'>Login</h1>
                                 <div className="row">
                                     <div className="col-12 mt-3">
                                         <label className='form-label d-block text-start' htmlFor="email">Email :</label>
-                                        <input className='form-control' type="text" name="email" id="email" placeholder='Enter your email'/>
+                                        <input 
+                                            onChange={formik.handleChange} 
+                                            value={formik.values.email} 
+                                            className='form-control' 
+                                            type="text" 
+                                            name="email" 
+                                            id="email" 
+                                            placeholder='Enter your email'/>
                                     </div>
                                     <div className="col-12 mt-3">
                                         <label className='form-label d-block text-start' htmlFor="password">Password :</label>
-                                        <input className='form-control' type="password" name="password" id="password" placeholder='Enter your password'/>
+                                        <input 
+                                            onChange={formik.handleChange} 
+                                            value={formik.values.password}  
+                                            className='form-control' 
+                                            type="password" 
+                                            name="password" id="password" 
+                                            placeholder='Enter your password'/>
                                     </div>
                                     <div className="col-12 mt-3">
                                         <button className='w-100 btn btn-primary' type="submit">Submit</button>
