cb88fdc5f29735ce31bd5eef5536f6b4f7d8c9eb
mohammad-nayem <nishanahmed13913@gmail.com>
2025-02-21 22:53:08 +0600

    Customize Bootstrap Loader and set Blog and Service section

--- client/src/Components/Main/BootstrapLoader.jsx
+++ client/src/Components/Main/BootstrapLoader.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const BootstrapLoader = () => {
     return (
         <>
-            <div className="bootstrap-loader w-100 vh-100 d-flex justify-content-center align-items-center">
+            <div className="bootstrap-loader ">
                 <div className="spinner-border text-info " role="status">
                     <span className="visually-hidden">Loading...</span>
                 </div>
--- client/src/Pages/Main/Service.jsx
+++ client/src/Pages/Main/Service.jsx
@@ -3,16 +3,22 @@ import React, { useEffect, useState } from 'react';
 import Layout from '../../Layout/Layout';
 import ServiceCard from '../../Components/Main/ServiceCard';
 import { ApiRequest } from '../../ApiRequest/Api';
+import BootstrapLoader from '../../Components/Main/BootstrapLoader';
 
 
 
 const Service = () => {
-    const [ service, setService] = useState([]);
 
+    // Mangae State
+    const [ service, setService] = useState([]);
+    const [loading, setLoading] = useState(true);
+    
+    // Call APi and Load Data
     useEffect(() => {
         (async () => {
             const result = await ApiRequest("GET", "/read-service");
             setService(result.data);
+            setLoading(false);
         })()
     }, [])
 
@@ -23,7 +29,10 @@ const Service = () => {
         <Layout>
             <section>
                 <div className="container py-5">
-                    <div className="row h-100 d-flex justify-content-center  ">
+
+                {
+                    loading ? (<BootstrapLoader />):(
+                        <div className="row h-100 d-flex justify-content-center  ">
                         <div className="col-md-12">
                             <h1 className='h1_color fw-bold text-center'>Our services</h1>
 
@@ -36,7 +45,10 @@ const Service = () => {
                                  }
                             </div>
                         </div>
-                    </div>
+                    </div>    
+                    )
+                }
+                    
                 </div>
             </section>
             
