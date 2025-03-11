0994db58f3bf308de9022447a4a0c78ba91fd7a3
mohammad-nayem <nishanahmed13913@gmail.com>
2025-03-11 20:09:56 +0600

    Change blog title color

--- client/src/Components/Main/Card.jsx
+++ client/src/Components/Main/Card.jsx
@@ -12,7 +12,7 @@ const Card = ({ data }) => {
                 <div style={{minWidth: '15rem', maxWidth: '17rem' , height: "auto"}} className="card-hover rounded rounded-3 border     border-sm border-secondary">
                     <img className='w-100 rounded rounded-3' src={  data.image } alt="" />
                     <div className="p-2  text-start">
-                        <h4 className='py-1 text-primary fw-bold'> { data.title } </h4>
+                        <h4 className='py-1 text-white fw-bold'> { data.title } </h4>
 
                         { data?.description && (
                             <div style={{maxWidth: "17rem"}} className=' text-start line-clamp line-spacing' dangerouslySetInnerHTML={{ __html: data.description }}></div>
